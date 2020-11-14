cca9cb366cefcb5fad4bc9f64ac38e9000910412
Cesar Fantini, Caio <ccesar@indracompany.com>
2020-11-14 18:07:12 -0300

    property adjustments

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -6,11 +6,3 @@ VALUES
 	('caio',/*caio2020*/ '$2a$10$u7weR.0orVbGP42/8TwToesWrFpWN0hQZ6kWy/L6zBp1v1w3r/Bey','USER'),
 	('isabela',/*isab2020*/ '$2a$10$ZJwwu7IuLHIWes5FNEDGF.o2t6opmBwCqguEAg85ptNgSb9jCzjm.','USER'),
 	('gabriel',/*gabi2020*/ '$2a$10$u9DfsE5pV.bVNjIsGcbppeVb70a/Y6dM.5XnAY1EcoWFSKJnPteJ2','USER');
-
--- tasks
-INSERT INTO 
-	TASK (user_id, created_date, change_date, summary, description, status) 
-VALUES
-  	(1, CURRENT_TIMESTAMP(), null, 'teste summary 1', 'teste description 1', 0),
-	(1, CURRENT_TIMESTAMP(), null, 'teste summary 2', 'teste description 2', 0),
-	(2, CURRENT_TIMESTAMP(), null, 'teste summary 3', 'teste description 3', 1);
